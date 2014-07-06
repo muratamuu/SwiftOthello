bdf5e247add8eefcef8d15e1a3b92c97e004edcc
muratamuu <murata.muu@gmail.com>
2014-07-06 15:00:16 +0900

    Initial Commit

---
+++ SwiftOthello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftOthello
+//
+//  Created by muratamuu on 2014/07/06.
+//  Copyright (c) 2014年 muratamuu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
