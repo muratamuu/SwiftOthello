421526499ea24ffde07384745d9bc3610933a71e
muratamuu <murata.muu@gmail.com>
2014-08-09 01:45:32 +0900

    Xcode beta4対応

--- SwiftOthello/OthelloView.swift
+++ SwiftOthello/OthelloView.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+let EMPTY = 0, BLACK_STONE = 1, WHITE_STONE = 2
+
 class OthelloView: UIView {
     let initboard = [
         [0,0,0,0,0,0,0,0,0,0],
@@ -21,92 +23,88 @@ class OthelloView: UIView {
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
     ];
-    let FREE = 0, BLACK_STONE = 1, WHITE_STONE = 2
-    var board = Int[][]()
-    var boardSide: CGFloat = 0
-    let origX: CGFloat = 0
-    var origY: CGFloat = 0
-    var lbl: String = ""
+    var board:[[Int]]
+    let white = UIColor.whiteColor().CGColor
+    let black = UIColor.blackColor().CGColor
+    let green = UIColor(red:0.6, green:1.0, blue:0.2, alpha:1.0).CGColor
+    var side:CGFloat
+    var top:CGFloat
+    let left:CGFloat = 0
+    let lbl:UILabel = UILabel()
     var isGameOver = false
-    var onceToken: dispatch_once_t = 0
-    let green = CGColorCreateGenericRGB(0.6, 1.0, 0.2, 1.0)
-    let white = CGColorCreateGenericRGB(1.0, 1.0, 1.0, 1.0)
-    let black = CGColorCreateGenericRGB(0.0, 0.0, 0.0, 1.0)
-    let blue  = CGColorCreateGenericRGB(0.6, 0.5, 1.0, 1.0)
     
-    func initialize () {
-        board = initboard.copy()
-        boardSide = self.frame.size.width / 8.0
-        origY = self.frame.size.height / 2.0 - (4 * boardSide)
-        lbl = ""
-        isGameOver = false
+    init(coder aDecoder: NSCoder!) {
+        let appFrame = UIScreen.mainScreen().applicationFrame
+        side = appFrame.size.width / 8
+        top = (appFrame.size.height - (side * 8)) / 2
+        board = initboard
+        
+        super.init(coder:aDecoder)
+        
+        lbl.text = ""
+        lbl.frame = CGRectMake(10, top / 2, appFrame.size.width, top / 2)
+        addSubview(lbl)
     }
     
     override func drawRect(rect: CGRect) {
-        dispatch_once(&onceToken, { self.initialize() })
-        
         let context = UIGraphicsGetCurrentContext()
         CGContextSetStrokeColorWithColor(context, white)
         CGContextSetLineWidth(context, 1.5)
         
-        for x in 1..9 {
-            for y in 1..9 {
-                // draw board
-                let xpos = origX + CGFloat(x-1) * boardSide
-                let ypos = origY + CGFloat(y-1) * boardSide
-                let r = CGRectMake(xpos, ypos, boardSide, boardSide)
+        for y in 1...8 {
+            for x in 1...8 {
+                let rx = left + side * CGFloat(x-1)
+                let ry = top + side * CGFloat(y-1)
+                let rect = CGRectMake(rx, ry, side, side)
                 CGContextSetFillColorWithColor(context, green)
-                CGContextFillRect(context, r)
-                CGContextStrokeRect(context, r)
+                CGContextFillRect(context, rect)
+                CGContextStrokeRect(context, rect)
                 
-                if board[x][y] == BLACK_STONE {
+                if board[y][x] == BLACK_STONE {
                     CGContextSetFillColorWithColor(context, black)
-                    CGContextFillEllipseInRect(context, r) // draw black stone
-                } else if board[x][y] == WHITE_STONE {
+                    CGContextFillEllipseInRect(context, rect) // draw black stone
+                } else if board[y][x] == WHITE_STONE {
                     CGContextSetFillColorWithColor(context, white)
-                    CGContextFillEllipseInRect(context, r) // draw white stone
+                    CGContextFillEllipseInRect(context, rect) // draw white stone
                 }
             }
         }
-        
-        // draw label
-        let cstr = lbl.bridgeToObjectiveC().UTF8String
-        CGContextSetTextDrawingMode(context, kCGTextFill)
-        CGContextSetFillColorWithColor(context, blue)
-        CGContextSelectFont(context, "Helvetica", 20 , kCGEncodingMacRoman)
-        CGContextSetTextMatrix(context, CGAffineTransformMake(1.0, 0.0, 0.0, -1.0, 0.0, 0.0));
-        CGContextShowTextAtPoint(context, 0, origY, cstr, strlen(cstr));
     }
     
     override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
-        if isGameOver { // restart
-            initialize()
+        if isGameOver {
+            board = initboard
+            updateGame()
             setNeedsDisplay()
             return
         }
-        
-        if canPlaced(BLACK_STONE).count > 0 { // player can put black stone.
+        if let canBlack = canPlaced(board, BLACK_STONE) { // player can put black stone.
             if let (x, y) = getPos(touches) {
-                if putStone(&board, x, y, BLACK_STONE) {
-                    cpuPut()
+                if let blackPlaces = flip(board, x, y, BLACK_STONE) {
+                    putStones(blackPlaces, stone: BLACK_STONE)
+                    if let whitePlaces = cpuFlip(board, WHITE_STONE) {
+                        putStones(whitePlaces, stone: WHITE_STONE)
+                    }
                 }
             }
         } else {
-            cpuPut()
+            if let whitePlaces = cpuFlip(board, WHITE_STONE) {
+                putStones(whitePlaces, stone: WHITE_STONE)
+            }
         }
-        
         updateGame()
         setNeedsDisplay()
     }
     
     func getPos(touches: NSSet!) -> (x: Int, y: Int)? {
-        let touch: UITouch = touches.anyObject() as UITouch
+        let touch:UITouch = touches.anyObject() as UITouch
         let point = touch.locationInView(self)
-        for x in 1..9 {
-            let xpos = origX + boardSide * CGFloat(x)
-            for y in 1..9 {
-                let ypos = origY + boardSide * CGFloat(y)
-                if point.x <= xpos && point.y <= ypos {
+        for y in 1...8 {
+            for x in 1...8 {
+                let rx = left + side * CGFloat(x-1)
+                let ry = top + side * CGFloat(y-1)
+                let rect = CGRectMake(rx, ry, side, side)
+                if CGRectContainsPoint(rect, point) {
                     return (x, y)
                 }
             }
@@ -114,101 +112,99 @@ class OthelloView: UIView {
         return nil
     }
     
-    func cpuPut() {
-        var places = canPlaced(WHITE_STONE) // cpu puts white stone.
-        if places.count > 0 {
-            let (x, y) = places[ Int(arc4random()) % places.count ]
-            putStone(&board, x, y, WHITE_STONE)
+    func putStones(places:[(Int, Int)], stone: Int) {
+        for (x, y) in places {
+            board[y][x] = stone
         }
     }
     
     func updateGame() {
-        let (free, black, white) = calcStones()
-        let canBlack = canPlaced(BLACK_STONE)
-        let canWhite = canPlaced(WHITE_STONE)
-        if free == 0 || (canBlack.isEmpty && canWhite.isEmpty) {
+        let (free, black, white) = calcStones(board)
+        let canBlack = canPlaced(board, BLACK_STONE)
+        let canWhite = canPlaced(board, WHITE_STONE)
+        if free == 0 || (!canBlack && !canWhite) {
+            lbl.text = "Game Over (Black:\(black) White:\(white))"
             isGameOver = true
-            lbl = "Game Over (black:\(black) white:\(white))"
-        } else if canBlack.isEmpty {
-            lbl = "Click to Skip Black"
-        } else if canWhite.isEmpty {
-            lbl = "Skipped White"
         } else {
-            lbl = ""
+            lbl.text = ""
+            isGameOver = false
         }
-    }
-    
-    func putStone (inout board: Int[][], _ x: Int, _ y: Int, _ stone: Int) -> Bool {
-        let b1 = flip(&board, x, y, stone, dx: 1, dy: 0)
-        let b2 = flip(&board, x, y, stone, dx:-1, dy: 0)
-        let b3 = flip(&board, x, y, stone, dx: 0, dy: 1)
-        let b4 = flip(&board, x, y, stone, dx: 0, dy:-1)
-        let b5 = flip(&board, x, y, stone, dx: 1, dy: 1)
-        let b6 = flip(&board, x, y, stone, dx:-1, dy:-1)
-        let b7 = flip(&board, x, y, stone, dx: 1, dy:-1)
-        let b8 = flip(&board, x, y, stone, dx:-1, dy: 1)
-        
-        let canPut = b1 || b2 || b3 || b4 || b5 || b6 || b7 || b8;
-        if canPut {
-            board[x][y] = stone
-        }
-        return canPut
-    }
-    
-    func flip (inout board: Int[][], _ x: Int, _ y: Int, _ stone: Int, dx: Int, dy: Int) -> Bool {
-        
-        var fliploop: (Int, Int) -> Bool = { _ in false }
-        fliploop = { (x: Int, y: Int) -> Bool in
-            if board[x][y] == self.FREE {
-                return false
-            } else if board[x][y] == stone {
-                return true
-            } else if fliploop (x+dx, y+dy) {
-                board[x][y] = stone // flip!
-                return true
-            } else {
-                return false
-            }
-        }
-        func reverse(stone: Int) -> Int {
-            return stone == BLACK_STONE ? WHITE_STONE : BLACK_STONE
-        }
-        
-        if board[x][y] == FREE && board[x+dx][y+dy] == reverse(stone) {
-            return fliploop(x+dx, y+dy)
-        } else {
-            return false
-        }
-    }
-    
-    func canPlaced(stone: Int) -> Array<(Int, Int)> {
-        var result = Array<(Int, Int)>()
-        for x in 1..9 {
-            for y in 1..9 {
-                if board[x][y] == 0 {
-                    var dummy = board.copy()
-                    if putStone(&dummy, x, y, stone) {
-                        result += (x, y)
-                    }
-                }
-            }
-        }
-        return result
-    }
-    
-    func calcStones () -> (free: Int, black: Int, white: Int) {
-        var free = 0, white = 0, black = 0
-        for i in 1..9 {
-            for j in 1..9 {
-                switch board[i][j] {
-                case BLACK_STONE: black++
-                case WHITE_STONE: white++
-                default: free++
-                }
-            }
-        }
-        return (free, black, white)
     }
 }
 
+func flip(board:[[Int]], x:Int, y:Int, stone:Int) -> [(Int, Int)]? {
+    if board[y][x] != EMPTY { return nil }
+    var result:[(Int, Int)] = []
+    result += flipLine(board, x, y, stone, 1, 0)
+    result += flipLine(board, x, y, stone,-1, 0)
+    result += flipLine(board, x, y, stone, 0, 1)
+    result += flipLine(board, x, y, stone, 0,-1)
+    result += flipLine(board, x, y, stone, 1, 1)
+    result += flipLine(board, x, y, stone,-1,-1)
+    result += flipLine(board, x, y, stone, 1,-1)
+    result += flipLine(board, x, y, stone,-1, 1)
+    if result.count > 0 {
+        result += (x, y)
+        return result
+    } else {
+        return nil
+    }
+}
 
+func flipLine(board:[[Int]], x:Int, y:Int, stone:Int, dx:Int, dy:Int) -> [(Int, Int)] {
+    var flipLoop: (Int, Int) -> [(Int, Int)]? = { _ in nil }
+    flipLoop = { (x: Int, y: Int) -> [(Int, Int)]? in
+        if board[y][x] == EMPTY {
+            return nil
+        } else if board[y][x] == stone {
+            return []
+        } else if var result = flipLoop(x+dx, y+dy) {
+            result += (x, y)
+            return result
+        } else {
+            return nil
+        }
+    }
+    if let result = flipLoop(x+dx, y+dy) {
+        return result
+    }
+    return []
+}
+
+func canPlaced(board:[[Int]], stone: Int) -> [(Int, Int)]? {
+    var result:[(Int, Int)] = []
+    for y in 1...8 {
+        for x in 1...8 {
+            if let res = flip(board, x, y, stone) {
+                result += (x, y)
+            }
+        }
+    }
+    if result.isEmpty {
+        return nil
+    } else {
+        return result
+    }
+}
+
+func cpuFlip(board:[[Int]], stone: Int) -> [(Int, Int)]? {
+    if let places = canPlaced(board, stone) {
+        let (x, y) = places[ Int(arc4random()) % places.count ]
+        return flip(board, x, y, stone)
+    }
+    return nil
+}
+
+func calcStones(board:[[Int]]) -> (free:Int, black:Int, white:Int) {
+    var free = 0, white = 0, black = 0
+    for y in 1...8 {
+        for x in 1...8 {
+            switch board[y][x] {
+            case BLACK_STONE: black++
+            case WHITE_STONE: white++
+            default: free++
+            }
+        }
+    }
+    return (free, black, white)
+}
